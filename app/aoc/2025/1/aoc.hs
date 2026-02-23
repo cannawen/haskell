72dbd0dbe18832ff97552d7cdb95531888efbfa6
Canna Wen <cannawen@gmail.com>
2026-02-23 06:57:46 -0500

    Parsing instructions into array

--- app/aoc/2025/1/aoc.hs
+++ app/aoc/2025/1/aoc.hs
@@ -1,8 +1,12 @@
 main :: IO ()
 main = do
     contents <- readFile "app/aoc/2025/1/input.txt"
-    let instructions = lines contents
-    let directions = map (\instruction -> instruction !! 0) instructions
-    let numbers = map tail instructions
-    print (take 5 directions)
-    print (take 5 numbers)
+    let instructionStrings = lines contents
+
+    let directions = map (\instruction -> instruction !! 0) instructionStrings
+    let numbers = map read (map tail instructionStrings) :: [Int]
+
+    let parsedInstruction = zip directions numbers
+
+    print (take 5 parsedInstruction)
+
