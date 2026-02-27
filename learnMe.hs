2924ea383b8d9c869442521e71d58fa5614a877c
Canna Wen <cannawen@gmail.com>
2026-02-27 12:05:26 -0500

    Fix spacing

--- learnMe.hs
+++ learnMe.hs
@@ -1,17 +1,17 @@
-doubleMe x = x + x  
-doubleUs x y = doubleMe x + doubleMe y 
+doubleMe x = x + x
+doubleUs x y = doubleMe x + doubleMe y
 
-doubleSmallNumber x = if x > 100  
-                        then x  
+doubleSmallNumber x = if x > 100
+                        then x
                         else x*2
 
 numbers = [1, 2, 3, 4] ++ [5, 6, 7, 8]
 words = "hello" ++ " " ++ ['w', 'o', 'r', 'l', 'd']
 
 -- More efficient; adding at the front
-prependNumbers = 5: [4, 3, 2, 1]
+prependNumbers = 5 : [4, 3, 2, 1]
 prependNumbers' = 5:4:3:2:1:[]
-prependWords = 'a': " lot of words"
+prependWords = 'a' : " lot of words"
 
 indexOut = prependNumbers !! 1
 
