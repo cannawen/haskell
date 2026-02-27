68fb46117cd332e3ddb9597b0d87f365c8313b5c
Canna Wen <cannawen@gmail.com>
2026-02-27 16:33:38 -0500

    Refactor to make code more haskell-y
    Co-Authored-By: Florian Ragwitz
    Co-Authored-By: c stavridis

--- app/aoc/2025/1/aoc.hs
+++ app/aoc/2025/1/aoc.hs
@@ -1,19 +1,25 @@
+
+-- parse contents = map (\(dir, num) -> if dir == 'R' then num else - num) parsedInstructions
+--     where instructionStrings = lines contents
+--           directions = map (\instruction -> instruction !! 0) instructionStrings
+--           numbers = map read (map tail instructionStrings) :: [Int]
+--           parsedInstructions = zip directions numbers
+
+parse :: String -> [Int]
+parse = map f . lines 
+  where f ('L':xs) = - read xs
+        f ('R':xs) = read xs
+
+part1 = snd . foldl 
+            (\(current, zeroCount) num -> (rem (current + num) 100, if rem (current + num) 100 == 0 then zeroCount + 1 else zeroCount)) 
+            (50, 0) 
+            
+
 main :: IO ()
 main = do
 --     https://adventofcode.com/2025/day/1
     contents <- readFile "app/aoc/2025/1/input.txt"
-    let instructionStrings = lines contents
+    let numbers = parse contents
 
-    let directions = map (\instruction -> instruction !! 0) instructionStrings
-    let numbers = map read (map tail instructionStrings) :: [Int]
-
-    let parsedInstructions = zip directions numbers
-    let numbers = map (\(dir, num) -> if dir == 'R' then num else - num) parsedInstructions
-
-    let answer = foldl 
-            (\(current, zeroCount) num -> (rem (current + num) 100, if rem (current + num) 100 == 0 then zeroCount + 1 else zeroCount)) 
-            (50, 0) 
-            numbers
-
-    print (snd answer)
+    print (part1 numbers)
 
