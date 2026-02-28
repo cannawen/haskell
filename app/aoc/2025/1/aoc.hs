0cd68d68ee1ddcbc0982b9770c0672e5f63ce2e3
Canna Wen <cannawen@gmail.com>
2026-02-28 11:19:58 -0500

    Use scanl which is like foldl except it keeps an array of memos

--- app/aoc/2025/1/aoc.hs
+++ app/aoc/2025/1/aoc.hs
@@ -1,25 +1,15 @@
-
--- parse contents = map (\(dir, num) -> if dir == 'R' then num else - num) parsedInstructions
---     where instructionStrings = lines contents
---           directions = map (\instruction -> instruction !! 0) instructionStrings
---           numbers = map read (map tail instructionStrings) :: [Int]
---           parsedInstructions = zip directions numbers
+--     https://adventofcode.com/2025/day/1
 
 parse :: String -> [Int]
 parse = map f . lines 
   where f ('L':xs) = - read xs
         f ('R':xs) = read xs
 
-part1 = snd . foldl 
-            (\(current, zeroCount) num -> (rem (current + num) 100, if rem (current + num) 100 == 0 then zeroCount + 1 else zeroCount)) 
-            (50, 0) 
-            
+part1 = length . filter (== 0) . scanl (\current num -> rem (current + num) 100) 50
 
 main :: IO ()
 main = do
---     https://adventofcode.com/2025/day/1
     contents <- readFile "app/aoc/2025/1/input.txt"
     let numbers = parse contents
-
     print (part1 numbers)
 
