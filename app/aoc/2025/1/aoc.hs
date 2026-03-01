a57d0d72017df8733658a0aacca984328d17a4ff
Canna Wen <cannawen@gmail.com>
2026-02-28 21:59:41 -0500

    Ummm... okay.

--- app/aoc/2025/1/aoc.hs
+++ app/aoc/2025/1/aoc.hs
@@ -1,3 +1,4 @@
+import qualified Data.Bifunctor
 --     https://adventofcode.com/2025/day/1
 
 parse :: String -> [Int]
@@ -7,7 +8,7 @@ parse = map f . lines
 
 part1 = length . filter (== 0) . scanl (\current num -> rem (current + num) 100) 50
 part2 = sum . map fst . scanl f (50, 0)
-  where f (zeros, current) num = ( abs ( fst ( divMod (current + num) 100)) , snd ( divMod (current + num) 100))
+  where f (zeros, current) num = Data.Bifunctor.first abs ( divMod (current + num) 100)
 
 main :: IO ()
 main = do
