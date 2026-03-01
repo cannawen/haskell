c5d3ff7d3e903575207922e4b17d9b116b8a21e6
Canna Wen <cannawen@gmail.com>
2026-02-28 19:07:36 -0500

    Pt. 2 compiles, but gets the wrong answer. Is the algorithm wrong?

--- app/aoc/2025/1/aoc.hs
+++ app/aoc/2025/1/aoc.hs
@@ -6,10 +6,13 @@ parse = map f . lines
         f ('R':xs) = read xs
 
 part1 = length . filter (== 0) . scanl (\current num -> rem (current + num) 100) 50
+part2 = sum . map fst . scanl f (50, 0)
+  where f (zeros, current) num = ( abs ( fst ( divMod (current + num) 100)) , snd ( divMod (current + num) 100))
 
 main :: IO ()
 main = do
     contents <- readFile "app/aoc/2025/1/input.txt"
     let numbers = parse contents
-    print (part1 numbers)
 
+    print (part1 numbers) 
+    print (part2 numbers)
