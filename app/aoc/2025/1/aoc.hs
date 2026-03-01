70a6bf6ad75df3008638f3a107cd98cbd20e2759
Canna Wen <cannawen@gmail.com>
2026-02-28 22:37:04 -0500

    Cleanup

--- app/aoc/2025/1/aoc.hs
+++ app/aoc/2025/1/aoc.hs
@@ -1,6 +1,3 @@
-import qualified Data.Bifunctor
---     https://adventofcode.com/2025/day/1
-
 parse :: String -> [Int]
 parse = map f . lines
   where f ('L':xs) = - read xs
@@ -11,6 +8,7 @@ part1 = length . filter (== 0) . scanl (\current num -> rem (current + num) 100)
 part2 numbers = part1 (expandOnes numbers)
   where expandOnes = concatMap (\n -> replicate (abs n) (signum n))
 
+--     https://adventofcode.com/2025/day/1
 main :: IO ()
 main = do
     contents <- readFile "app/aoc/2025/1/input.txt"
