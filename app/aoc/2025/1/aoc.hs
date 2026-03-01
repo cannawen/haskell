cfa61e23b630afc245d8d6ab95214981ee732d3e
Canna Wen <cannawen@gmail.com>
2026-02-28 22:04:07 -0500

    Whoops

--- app/aoc/2025/1/aoc.hs
+++ app/aoc/2025/1/aoc.hs
@@ -7,7 +7,7 @@ parse = map f . lines
         f ('R':xs) = read xs
 
 part1 = length . filter (== 0) . scanl (\current num -> rem (current + num) 100) 50
-part2 = sum . map fst . scanl f (50, 0)
+part2 = sum . map fst . scanl f (0, 50)
   where f (zeros, current) num = Data.Bifunctor.first abs ( divMod (current + num) 100)
 
 main :: IO ()
