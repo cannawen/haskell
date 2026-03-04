8496682feee2ca2b33a9a3793b34a0c5bf12b5c2
Canna Wen <cannawen@gmail.com>
2026-03-04 11:32:44 -0500

    Operator trick? idk what looks better. Maybe I should finish the problem before refactoring for style.

--- app/aoc/2025/2/aoc.hs
+++ app/aoc/2025/2/aoc.hs
@@ -1,11 +1,13 @@
 import Data.List.Split (splitOn)
+import Data.Function ((&))
 
 parse :: [Char] -> [[Int]]
 parse input = map (map read . splitOn "-") (splitOn "," input)
 
 part1 :: [[Int]] -> [String]
--- part1 = map show $ concatMap (\[from, to] -> [from..to])
-part1 input = [show n | [start, end] <- input, n <- [start..end]]
+part1 input = input
+  & concatMap (\[from, to] -> [from..to])
+  & map show
 
 --     https://adventofcode.com/2025/day/2
 main :: IO ()
