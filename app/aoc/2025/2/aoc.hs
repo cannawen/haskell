0fba9e9ef0d0f82248c1cc60c896df6112233d54
Canna Wen <cannawen@gmail.com>
2026-03-04 10:37:56 -0500

    Add Day 2 puzzle

---
+++ app/aoc/2025/2/aoc.hs
@@ -0,0 +1,18 @@
+import Data.List.Split (splitOn)
+
+parse input = (splitOn "," input)
+
+part1 = length . filter (== 0) . scanl (\current num -> rem (current + num) 100) 50
+
+part2 numbers = part1 (expandOnes numbers)
+  where expandOnes = concatMap (\n -> replicate (abs n) (signum n))
+
+--     https://adventofcode.com/2025/day/2
+main :: IO ()
+main = do
+    contents <- readFile "app/aoc/2025/2/input-mini.txt"
+    let numbers = parse contents
+    print numbers
+
+    -- print (part1 numbers)
+    -- print (part2 numbers)
