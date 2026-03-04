14605a91f9ba9c488874b7ceb6e27d3c662b4667
Canna Wen <cannawen@gmail.com>
2026-03-04 13:37:21 -0500

    Logical issue. It needs to have 0 remainder for it to be a valid substring

--- app/aoc/2025/2/aoc.hs
+++ app/aoc/2025/2/aoc.hs
@@ -17,7 +17,7 @@ part1 input = input
 hasRepeatingDigits :: [Char] -> Bool
 hasRepeatingDigits string = -- "foobar"
   let stringLength = length string -- 6
-      possibleSubstrLengths = [1..stringLength `div` 2] -- [1, 2, 3]
+      possibleSubstrLengths = [x | x<- [1..stringLength `div` 2], mod stringLength x == 0] -- [1, 2, 3]
       possibleSubstrings = map (\length -> fst $ splitAt length string) possibleSubstrLengths -- ["f", "fo", "foo"]
       possibleSubstringRepeated = map (\str -> take stringLength (cycle str)) possibleSubstrings -- ["ffffff", "fofofo", "foofoo"]
   in 
