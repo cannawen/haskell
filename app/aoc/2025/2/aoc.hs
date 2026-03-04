da1ec3edc89cfc2a92f0328d1594f141050704f1
Canna Wen <cannawen@gmail.com>
2026-03-04 11:46:04 -0500

    This makes a lot more sense to me. Even though the IDE is yelling at me I'm writing two maps in a row.

--- app/aoc/2025/2/aoc.hs
+++ app/aoc/2025/2/aoc.hs
@@ -4,10 +4,15 @@ import Data.Function ((&))
 parse :: [Char] -> [[Int]]
 parse input = map (map read . splitOn "-") (splitOn "," input)
 
-part1 :: [[Int]] -> [String]
+part1 :: [[Int]] -> Int
 part1 input = input
   & concatMap (\[from, to] -> [from..to])
   & map show
+  & map (\numberString -> splitAt (length numberString `div` 2) numberString)
+  & filter (\(firstHalf, lastHalf) -> firstHalf == lastHalf)
+  & map (\(halfNum, _) -> halfNum ++ halfNum)
+  & map read
+  & sum
 
 --     https://adventofcode.com/2025/day/2
 main :: IO ()
