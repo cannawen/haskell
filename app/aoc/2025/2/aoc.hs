f53682a32fdc8c849ea150e0215eae6e860ec2db
Canna Wen <cannawen@gmail.com>
2026-03-04 11:07:47 -0500

    Turning integers back into strings. Very hard to read now.

--- app/aoc/2025/2/aoc.hs
+++ app/aoc/2025/2/aoc.hs
@@ -3,8 +3,8 @@ import Data.List.Split (splitOn)
 parse :: [Char] -> [[Int]]
 parse input = map (map read . splitOn "-") (splitOn "," input)
 
-part1 :: [[Int]] -> [Int]
-part1 input = concatMap (\pair -> [(pair !! 0)..(pair !! 1)]) input
+part1 :: [[Int]] -> [String]
+part1 input = map show (concatMap (\pair -> [(pair !! 0)..(pair !! 1)]) input)
 
 --     https://adventofcode.com/2025/day/2
 main :: IO ()
