700f76a2a5f5eef5d5763d8153bbdc2fb611183d
Canna Wen <cannawen@gmail.com>
2026-03-06 16:13:11 -0500

    Solved AoC 2025 Day 3 Pt. 1

--- app/aoc/2025/3/aoc.hs
+++ app/aoc/2025/3/aoc.hs
@@ -1,20 +1,27 @@
 import Data.List.Split (splitOn)
 import Data.Function ((&))
 import Data.Char (digitToInt)
+import Data.List (elemIndex, findIndex)
 
-
+import Data.Maybe (fromMaybe)
 parse input = lines input
   & map (map digitToInt)
 
+findJoltage intArray = maxInt * 10 + onesDigit
+  where maxInt = maximum (init intArray)
+        maxIndex = fromMaybe (error "") (elemIndex maxInt intArray)
+        onesDigit = maximum (drop (succ maxIndex) intArray)
 
+part1 content = content
+  & map findJoltage
+  & sum
 
 --     https://adventofcode.com/2025/day/3
 main :: IO ()
 main = do
-  contents <- readFile "app/aoc/2025/3/input-mini.txt"
+  contents <- readFile "app/aoc/2025/3/input.txt"
 
   let parsedContent = parse contents
-  print parsedContent
 
-  -- print (part1 parsedContent)
+  print (part1 parsedContent)
   -- print (part2 parsedContent)
