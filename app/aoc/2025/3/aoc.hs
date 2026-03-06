d7177cc72d1adba1b34a4391f68fbdb69d6af29a
Canna Wen <cannawen@gmail.com>
2026-03-06 17:49:32 -0500

    OK, this is compiling, but it's not getting the sub-array when you pick off the initial int

--- app/aoc/2025/3/aoc.hs
+++ app/aoc/2025/3/aoc.hs
@@ -12,6 +12,24 @@ findJoltage intArray = maxInt * 10 + onesDigit
         maxIndex = fromMaybe (error "") (elemIndex maxInt intArray)
         onesDigit = maximum (drop (succ maxIndex) intArray)
 
+findLeftmostMaximum :: [Int] -> (Int, [Int])
+findLeftmostMaximum integerList = (maxInt, remainingIntegers)
+  where maxInt = maximum integerList
+        maxIndex = fromMaybe (error "") (elemIndex maxInt integerList)
+        remainingIntegers = drop (succ maxIndex) integerList
+
+dropLast n = reverse . drop n . reverse
+
+findLeftmostMaximumWithDigits :: Int -> [Int] -> (Int, [Int])
+findLeftmostMaximumWithDigits 0 _ = (0, [])
+findLeftmostMaximumWithDigits n integerList = ((fst result) * (10 ^ (pred n)) , snd result)
+  where result = findLeftmostMaximum (dropLast n integerList)
+
+part2 content = content
+  & map (\intList -> ( map (\d -> findLeftmostMaximumWithDigits d intList ) (reverse [0..12])))
+  & map (map fst)
+  & map sum
+
 part1 content = content
   & map findJoltage
   & sum
@@ -19,9 +37,9 @@ part1 content = content
 --     https://adventofcode.com/2025/day/3
 main :: IO ()
 main = do
-  contents <- readFile "app/aoc/2025/3/input.txt"
+  contents <- readFile "app/aoc/2025/3/input-mini.txt"
 
   let parsedContent = parse contents
 
   print (part1 parsedContent)
-  -- print (part2 parsedContent)
+  print (part2 parsedContent)
