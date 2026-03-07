dc182564f9b3435d2efd8ce7c8aa62e3776592a9
Canna Wen <cannawen@gmail.com>
2026-03-07 08:37:19 -0500

    Construct matrix representing grid

--- app/aoc/2025/4/aoc.hs
+++ app/aoc/2025/4/aoc.hs
@@ -2,7 +2,11 @@ import Data.Function ((&))
 
 parse input = lines input
 
-part1 input = input
+part1 input = matrix
+  where rowSize = length input
+        columnSize =  head input & length
+        matrix = [(x,y) | x <- [0..pred rowSize], y <- [0..pred columnSize]]
+
 part2 input = input
 
 --     https://adventofcode.com/2025/day/4
