87179fff8018b80b6fbe59760aa903de89231cfd
Canna Wen <cannawen@gmail.com>
2026-03-07 09:38:16 -0500

    Solved Pt 1 (but it takes a moment) .. also pretty unhappy about state management here

--- app/aoc/2025/4/aoc.hs
+++ app/aoc/2025/4/aoc.hs
@@ -4,19 +4,21 @@ import Data.Maybe (fromMaybe)
 parse input = lines input
 
 part1 :: [String] -> String
-part1 input = 
+part1 input =
   foldl (\memo (x, y) ->
-          let mapValue = fromMaybe '?' (get x y) 
-          in if mapValue == '@' then memo else succ memo
+          let neighborIndexMatrix = [(nx,ny) | nx <- [(x-1)..(x+1)], ny <- [(y-1)..(y+1)], (nx, ny) /= (x,y)]
+              neighborValues = map (\(x,y) -> get x y) neighborIndexMatrix
+              canFitForklift = get x y  == Just '@' && (filter (== Just '@') neighborValues & length) < 4
+          in if canFitForklift then succ memo else memo
         ) 0 matrix
   & show
-  
+
   where rowNum = length input
         rowSize =  head input & length
         longInput = concat input
-        get x y = 
-          if x < 0 || y < 0 || x >= rowSize || y >= rowNum 
-            then Nothing 
+        get x y =
+          if x < 0 || y < 0 || x >= rowSize || y >= rowNum
+            then Nothing
             else Just (longInput !! (y * rowSize + x))
         matrix = [(x,y) | x <- [0..pred rowSize], y <- [0..pred rowNum]]
 
@@ -25,7 +27,7 @@ part2 input = input
 --     https://adventofcode.com/2025/day/4
 main :: IO ()
 main = do
-  contents <- readFile "app/aoc/2025/4/input-mini.txt"
+  contents <- readFile "app/aoc/2025/4/input.txt"
 
   let parsedContent = parse contents
 
