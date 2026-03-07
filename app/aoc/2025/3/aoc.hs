d5a99f97a68706d6523120492b96cda31d49f657
Canna Wen <cannawen@gmail.com>
2026-03-06 21:08:26 -0500

    Trying to reduce over the last 12 digits of the array, and cut down the remainder of the array as appropriate. Also need to keep track of the digit.

--- app/aoc/2025/3/aoc.hs
+++ app/aoc/2025/3/aoc.hs
@@ -20,40 +20,21 @@ dropElems first last array =
   & drop last
   & reverse
 
+dropLast last = reverse . drop last . reverse
+
 findMaximum intArray = maxIndex
   where maxInt = maximum intArray
         maxIndex = fromMaybe (error "") (elemIndex maxInt intArray)
 
-find12Joltage intArray = 
- firstDigit * 10^11 
-  + secondDigit * 10^10
-  + thirdDigit * 10^9
-  + fourthDigit * 10^8
-  + fifthDigit * 10^7
-  + sixthDigit * 10^6
-  + seventhDigit * 10^5
-  + eighthDigit * 10^4
-  + ninthDigit * 10^3
-  + tenthDigit * 10^2
-  + eleventhDigit * 10^1
-  + twelfthDigit * 10^0
+foldFn :: ([Int], [Int]) -> Int -> ([Int], [Int]) 
+foldFn memo newElement = (drop index newArray, [newArray !! index])
+  where newArray = (fst memo) ++ [newElement]
+        index = findMaximum newArray
 
-  where 
-    firstDigit = findMaximum (dropElems 0 11 intArray)
-    secondDigit =  findMaximum (dropElems firstDigit 10 intArray)
-    thirdDigit =  findMaximum (dropElems secondDigit 9 intArray)
-    fourthDigit = findMaximum (dropElems thirdDigit 8 intArray)
-    fifthDigit    = findMaximum (dropElems fourthDigit 7 intArray)
-    sixthDigit    = findMaximum (dropElems fifthDigit 6 intArray)
-    seventhDigit  = findMaximum (dropElems sixthDigit 5 intArray)
-    eighthDigit   = findMaximum (dropElems seventhDigit 4 intArray)
-    ninthDigit    = findMaximum (dropElems eighthDigit 3 intArray)
-    tenthDigit    = findMaximum (dropElems ninthDigit 2 intArray)
-    eleventhDigit = findMaximum (dropElems tenthDigit 1 intArray)
-    twelfthDigit  = findMaximum (dropElems eleventhDigit 0 intArray)
+find12Joltage' intArray = foldl foldFn (intArray,[]) (reverse (take 12 (reverse intArray)))
 
 part2 content = content
-  & map find12Joltage
+  & map find12Joltage'
 
 part1 content = content
   & map findJoltage
