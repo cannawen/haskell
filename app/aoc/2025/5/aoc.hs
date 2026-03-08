d44932a784548c0f9367bb6ec966ec8112b5f5a2
Canna Wen <cannawen@gmail.com>
2026-03-08 13:42:00 -0400

    Refactor to use splitOn instead of break for initial parsing
    Co-Authored-By: Rafal Dittwald <rafal.dittwald@gmail.com>

--- app/aoc/2025/5/aoc.hs
+++ app/aoc/2025/5/aoc.hs
@@ -7,9 +7,9 @@ import Data.Tuple (swap)
 
 parse input = (parseRanges ranges, parseFoods foods)
 
-  where splitIndex = fromMaybe 0 (elemIndex "" (lines input))
-        ranges = take splitIndex (lines input)
-        foods = drop (splitIndex + 1) (lines input)
+  where r:f:_ = splitOn "\n\n" input
+        ranges = lines r
+        foods = lines f
 
         parseRanges :: [String] -> [(Int, Int)]
         parseRanges ranges = ranges
