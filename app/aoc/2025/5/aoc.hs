72b6cdf88175bdb385bf52414023196ff11fa788
Canna Wen <cannawen@gmail.com>
2026-03-08 12:36:38 -0400

    Attempt #1 - need to fix up last element of the memo

--- app/aoc/2025/5/aoc.hs
+++ app/aoc/2025/5/aoc.hs
@@ -1,6 +1,8 @@
 import Data.Function ((&))
-import Data.List (elemIndex)
+import Data.List (elemIndex, sort)
 import Data.Maybe (fromMaybe)
+import qualified Data.Set as Set
+import Data.Tuple (swap)
 
 parse input = (parseRanges ranges, parseFoods $ tail foods)
   where splitIndex = fromMaybe 0 (elemIndex "" (lines input))
@@ -14,18 +16,34 @@ parse input = (parseRanges ranges, parseFoods $ tail foods)
         parseFoods :: [String] -> [Int]
         parseFoods foods = map read foods
 
-part1 (validRanges, foodSet) = filter (\food -> inRange food validRanges) foodSet & length & show
+part1 (validRanges, food) = filter (\food -> inRange food validRanges) food & length & show
   where inRange food validRanges = validRanges
           & any (\range -> food >= fst range && food <= snd range)
 
-part2 input = input
+merge :: (Int, Int) -> (Int, Int) -> [(Int, Int)]
+merge range1 range2 = if e1 < s2 then [r1, r2] else 
+                      if e1 == s2 then [(s1, e2)] else 
+                      if e2 < e1 then [r1] else 
+                      if s2 < e1 then [(s1, e2)] else
+                      [r1, r2]
+  where [r1, r2] = sort [range1, range2]
+        s1 = fst r1
+        e1 = snd r1
+        s2 = fst r2
+        e2 = snd r2
+
+part2 (ranges, _) = foldl (\memo range -> concatMap (\mem -> merge range mem) memo) [head rangeStartToEnd] (tail rangeStartToEnd)
+
+  where rangeStartToEnd = sort ranges
+        rangesEndToStart = map swap ranges & sort
 
 --     https://adventofcode.com/2025/day/5
 main :: IO ()
 main = do
-  contents <- readFile "app/aoc/2025/5/input.txt"
+  contents <- readFile "app/aoc/2025/5/input-mini.txt"
 
   let parsedContent = parse contents
+  print parsedContent
 
   print $ part1 parsedContent
   print $ part2 parsedContent
