5c848fa439dc79fe07e15a4083e01d664c9bc419
Canna Wen <cannawen@gmail.com>
2026-03-08 16:30:46 -0400

    Parse math matrix and transpose

--- app/aoc/2025/6/aoc.hs
+++ app/aoc/2025/6/aoc.hs
@@ -1,17 +1,24 @@
 import Data.Function ((&))
+import Data.List.Split (splitOn)
+import Data.List (transpose)
 
-parse input = input
+parse input = (lines input)
+  & map (splitOn " ")
+  & map (filter (/= ""))
+  & transpose
+  & map reverse
 
-part1 input = input
+part1 input = "pt1"
 
-part2 input = input
+part2 input = "pt2"
 
 --     https://adventofcode.com/2025/day/6
 main :: IO ()
 main = do
-  contents <- readFile "app/aoc/2025/6/input.txt"
+  contents <- readFile "app/aoc/2025/6/input-mini.txt"
 
   let parsedContent = parse contents
+  print $ parsedContent
 
   print $ part1 parsedContent
   print $ part2 parsedContent
