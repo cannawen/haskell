9f9c7e3bc9b772dfa5035c686f4898ae223433e6
Canna Wen <cannawen@gmail.com>
2026-03-08 16:45:06 -0400

    Solve Day 6 Pt 1

--- app/aoc/2025/6/aoc.hs
+++ app/aoc/2025/6/aoc.hs
@@ -8,17 +8,23 @@ parse input = (lines input)
   & transpose
   & map reverse
 
-part1 input = "pt1"
+part1 input = input
+  & map calculate
+  & sum
+
+  where getOperator mathArray = if head mathArray == "+" then (+) else (*)
+        getNumbers mathArray = map read (tail mathArray)
+        calculate mathArray = foldl (getOperator mathArray) (head $ getNumbers mathArray) (tail $ getNumbers mathArray)
 
 part2 input = "pt2"
 
 --     https://adventofcode.com/2025/day/6
 main :: IO ()
 main = do
-  contents <- readFile "app/aoc/2025/6/input-mini.txt"
+  contents <- readFile "app/aoc/2025/6/input.txt"
 
   let parsedContent = parse contents
-  print $ parsedContent
+  -- print $ parsedContent
 
   print $ part1 parsedContent
   print $ part2 parsedContent
