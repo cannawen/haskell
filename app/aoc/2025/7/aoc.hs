b9c421eab1f9e141612f6ac51852972652d1b845
Canna Wen <cannawen@gmail.com>
2026-03-08 21:34:54 -0400

    Attempt pt 2 - incorrect
    Co-Authored-By: Rafal Dittwald <rafal.dittwald@gmail.com>

--- app/aoc/2025/7/aoc.hs
+++ app/aoc/2025/7/aoc.hs
@@ -3,6 +3,11 @@ import Data.List.Split (splitOn, splitWhen)
 import Data.List (elemIndices)
 import qualified Data.Text as T
 import qualified Data.List.Split.Internals as T
+import qualified Data.Map as Map
+
+-- Equivalent to Clojure's frequencies
+frequencies :: Ord a => [a] -> Map.Map a Int
+frequencies xs = Map.fromListWith (+) [(x, 1) | x <- xs]
 
 modifyCurrentRow prevRow row = 
   let findHit = map (\(prev, curr) -> if prev == 'S' && curr == '.' then 'S' else 
@@ -18,7 +23,8 @@ part1 input = scanl1 modifyCurrentRow input -- & concat & filter (=='h') & lengt
 
 ------
 
-ray = -1
+ray = 1
+hitSplitter = -1
 splitter = -2
 newline = -3
 
@@ -30,16 +36,16 @@ parse input = splitOn [newline] (map (\c -> case c of
                                       _ -> newline) input)
 
 modifyCurrentRow' prevRow row = 
-  let findHit = map (\(prev, curr) -> if prev == 'S' && curr == '.' then 'S' else 
-                                     if prev == 'S' && curr == '^' then 'h' else 
-                                     curr) (zip prevRow row)
-      hIndices = elemIndices 'h' findHit
-      newSIndices = concatMap (\i -> [i-1, i+1]) hIndices
-      newRow = map (\(i, curr) -> if elem i newSIndices then 'S' else curr) (zip [0..] findHit)
+  let findHit = map (\(prev, curr) -> if prev > 0 && curr == 0 then prev else 
+                                      if prev > 0 && curr == splitter then hitSplitter else 
+                                      curr) (zip prevRow row)
+      hitSplitterIndices = elemIndices hitSplitter findHit
+      newRayIndices = Map.unionsWith (+) (map (\i -> Map.fromList [(i-1,prevRow!!i),(i+1,prevRow!!i)]) hitSplitterIndices)
+      newRow = map (\(i, curr) -> Map.findWithDefault curr i newRayIndices) (zip [0..] findHit)
 
   in newRow
 
-part2 input = input
+part2 input = scanl1 modifyCurrentRow' input
 
 --     https://adventofcode.com/2025/day/7
 main :: IO ()
