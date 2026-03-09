1e299a6fc2094ca395ad9c51d82cd97c2473f1bb
Canna Wen <cannawen@gmail.com>
2026-03-08 20:05:36 -0400

    Solved pt 1

--- app/aoc/2025/7/aoc.hs
+++ app/aoc/2025/7/aoc.hs
@@ -1,18 +1,28 @@
 import Data.Function ((&))
 import Data.List.Split (splitOn, splitWhen)
-import Data.List (transpose)
+import Data.List (elemIndices)
 import qualified Data.Text as T
 import qualified Data.List.Split.Internals as T
 
 parse input = lines input
 
-part1 input = "pt1"
+modifyCurrentRow prevRow row = 
+  let findHit = map (\(prev, curr) -> if prev == 'S' && curr == '.' then 'S' else 
+                                     if prev == 'S' && curr == '^' then 'h' else 
+                                     curr) (zip prevRow row)
+      hIndices = elemIndices 'h' findHit
+      newSIndices = concatMap (\i -> [i-1, i+1]) hIndices
+      newRow = map (\(i, curr) -> if elem i newSIndices then 'S' else curr) (zip [0..] findHit)
+
+  in newRow
+
+part1 input = scanl1 (\prevRow row -> modifyCurrentRow prevRow row) input & concat & filter (=='h') & length
 part2 input = "pt2"
 
---     https://adventofcode.com/2025/day/6
+--     https://adventofcode.com/2025/day/7
 main :: IO ()
 main = do
-  contents <- readFile "app/aoc/2025/6/input.txt"
+  contents <- readFile "app/aoc/2025/7/input.txt"
 
   parse contents & part1 & print
   parse contents & part2 & print
