43c29cc41fc0eab89537af38c1b6837d3550d896
Canna Wen <cannawen@gmail.com>
2026-03-09 17:47:57 -0400

    Creating Row and Grid types

--- app/aoc/2025/7.2/aoc.hs
+++ app/aoc/2025/7.2/aoc.hs
@@ -5,12 +5,18 @@ data Tile
     | Path Int
     deriving (Show, Eq)
 
+newtype Row  = Row [Tile] deriving (Show, Eq)
+newtype Grid = Grid [Row] deriving (Show, Eq)
+
+parse :: String -> Grid
 parse contents = lines contents
     & map (map (\c ->
         if c == 'S' then Path 1 else
         if c == '^' then Splitter else
         Path 0 ))
     & filter (any (/= Path 0))
+    & map Row
+    & Grid
 
 --     https://adventofcode.com/2025/day/7
 main :: IO ()
