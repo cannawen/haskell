c76eef767f2dca4d68e07c8b06009c64aa774fb3
Canna Wen <cannawen@gmail.com>
2026-03-09 10:49:20 -0400

    Filter unused blank lines

--- app/aoc/2025/7/aoc.hs
+++ app/aoc/2025/7/aoc.hs
@@ -1,19 +1,18 @@
 import Data.Function ((&))
 import Data.List.Split (splitOn, splitWhen)
-import Data.List (elemIndices)
+import Data.List ( elemIndices, intercalate )
 import qualified Data.Text as T
 import qualified Data.List.Split.Internals as T
 import qualified Data.Map as Map
 import Text.Show.Pretty (pPrint)
-import Data.List (intercalate)
 
 -- Equivalent to Clojure's frequencies
 frequencies :: Ord a => [a] -> Map.Map a Int
 frequencies xs = Map.fromListWith (+) [(x, 1) | x <- xs]
 
-modifyCurrentRow prevRow row = 
-  let findHit = map (\(prev, curr) -> if prev == 'S' && curr == '.' then 'S' else 
-                                     if prev == 'S' && curr == '^' then 'h' else 
+modifyCurrentRow prevRow row =
+  let findHit = map (\(prev, curr) -> if prev == 'S' && curr == '.' then 'S' else
+                                     if prev == 'S' && curr == '^' then 'h' else
                                      curr) (zip prevRow row)
       hIndices = elemIndices 'h' findHit
       newSIndices = concatMap (\i -> [i-1, i+1]) hIndices
@@ -31,28 +30,30 @@ splitter = -2
 
 parse :: String -> [[Int]]
 parse input = map (map convertToInt) (lines input)
-  where convertToInt c = case c of 
+  & filter (\line -> sum line /= 0)
+  where convertToInt c = case c of
                           'S' -> ray
                           '^' -> splitter
                           _ -> 0
 
-modifyCurrentRow' prevRow row = 
-  let findHit = map (\(prev, curr) -> if prev > 0 && curr == 0 then prev else 
-                                      if prev > 0 && curr == splitter then hitSplitter else 
+modifyCurrentRow' prevRow row =
+  let findHit = map (\(prev, curr) -> if prev > 0 && curr == 0 then prev else
+                                      if prev > 0 && curr == splitter then hitSplitter else
                                       curr) (zip prevRow row)
       hitSplitterIndices = elemIndices hitSplitter findHit
       newRayIndices = Map.unionsWith (+) (map (\i -> Map.fromList [(i-1,prevRow!!i),(i+1,prevRow!!i)]) hitSplitterIndices)
-      newRow = map (\(i, curr) -> 
+      newRow = map (\(i, curr) ->
         if curr < 0 then
           curr
         else
-          Map.findWithDefault 0 i newRayIndices 
+          Map.findWithDefault 0 i newRayIndices
           + if prevRow!!i > 0 then prevRow!!i else 0
           ) (zip [0..] findHit)
 
   in newRow
 
-part2 input = foldl1 modifyCurrentRow' input & sum
+part2 input = foldl1 modifyCurrentRow' input & filter (>0) & sum
+-- part2 input = scanl1 modifyCurrentRow' input -- & sum
 
 
 prettyMatrix :: [[Int]] -> String
