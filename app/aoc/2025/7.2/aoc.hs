03ec23365a6085b2fa17e2d2fd538262564a0fad
Canna Wen <cannawen@gmail.com>
2026-03-10 12:53:57 -0400

    Override default Show fn for Tiles

--- app/aoc/2025/7.2/aoc.hs
+++ app/aoc/2025/7.2/aoc.hs
@@ -4,7 +4,12 @@ import Data.List (sort, intercalate)
 data Tile
     = Splitter
     | Path Int
-    deriving (Show, Eq, Ord)
+    deriving (Eq, Ord)
+
+instance Show Tile where
+    show Splitter = "^"
+    show (Path 0) = "."
+    show (Path n) = show n
 
 combineTiles :: (Int, Tile, Tile) -> [(Int, Tile)]
 combineTiles (i, Path prev, Path curr) = [(i, Path (prev + curr))]
@@ -44,30 +49,10 @@ calculateNewRow prevRow currRow = map snd collated
 part2 :: Grid -> Int
 part2 grid = sumRow (foldl1 calculateNewRow grid)
 
--- padLeft :: Int -> String -> String
--- padLeft w s = replicate (max 0 (w - length s)) ' ' ++ s
-
--- prettyGrid :: Grid -> String
--- prettyGrid grid =
---     let
---         maxVal = maximum (0 : [n | row <- grid, Path n <- row])
---         cellW = max 1 (length (show maxVal))
---         fmtTile t = case t of
---             Splitter -> padLeft cellW "^"
---             Path 0 -> padLeft cellW "."
---             Path n -> padLeft cellW (show n)
---         maxCols = maximum (0 : map length grid)
---         header =
---             "r\\c | " ++ intercalate " " [padLeft cellW (show c) | c <- [0 .. maxCols - 1]]
---         divider = replicate (length header) '-'
---         fmtRow (r, row) =
---             padLeft 3 (show r) ++ " | " ++ intercalate " " (map fmtTile row)
---     in
---         unlines (header : divider : map fmtRow (zip [0..] grid))
+-- part2 grid = scanl1 calculateNewRow grid
 
 --     https://adventofcode.com/2025/day/7
 main :: IO ()
 main = do
     contents <- readFile "app/aoc/2025/7/input-mini.txt"
-    -- putStrLn (prettyGrid (scanl1 calculateNewRow (parse contents)))
     print $ part2 $ parse contents
