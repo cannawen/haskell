19d74835ceb11073155765981fb04c171f4ee1b1
Canna Wen <cannawen@gmail.com>
2026-03-13 13:24:18 -0400

    p107 - Modules, submodules.

--- module.hs
+++ module.hs
@@ -1,6 +1,13 @@
-import Data.List (words, sort, group)
+import Data.List
 import Data.Function ((&))
+import Data.Char
 
 w x = x & words & sort & group & map (\arr -> (head arr, length arr))
 w' = map (\arr -> (head arr, length arr)) . group . sort . words
 
+find' :: Eq a => [a] -> [a] -> Bool
+find' needle haystack = tails haystack
+    & foldr (\partialHaystack memo -> memo || length (filter (\(n, h) -> n==h) (zip needle partialHaystack)) == length needle) False
+
+encode :: Int -> String -> String
+encode i = map (\input -> ord input + i & chr )
