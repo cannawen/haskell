a8ad42953ff9ae20d78724c1f1516ec0e1489b9f
Canna Wen <cannawen@gmail.com>
2026-03-27 14:23:45 -0400

    Jesus Christ

--- app/aoc/2025/8/aoc.hs
+++ app/aoc/2025/8/aoc.hs
@@ -3,34 +3,42 @@ import Data.List.Split
 import Data.List
 import qualified Data.Set as Set
 
-parse :: String -> [(Int, Int, Int)]
+-- type Point = (Int, Int, Int)
+data Point = Point
+  { x :: Double
+  , y :: Double
+  , z :: Double
+  } deriving (Show, Eq)
+type Circuit = Set.Set Point
+
+parse :: String -> [Point]
 parse input = 
     lines input
     & map (splitOn ",") 
     & map (\point -> map read point)
-    & map (\arr -> (head arr, arr !! 1, arr !! 2))
+    & map (\arr -> Point (head arr) (arr !! 1) (arr !! 2))
 
-mergePointsIntoSets :: Set.Set (Int, Int, Int) -> (Int, Int, Int) -> (Int, Int, Int)
-mergePointsIntoSets setSet p1 p2 = 
-    if setsToMerge == []
-    then Set.insert (Set.fromList [p1, p2]) setSet
-    else setSet
-    where setsToMerge = Set.filter (\set -> Set.member p1 set || Set.member p2 set) setSet 
+distance p1 p2 = (x p1 - x p2)^2 + (y p1 - y p2)^2 + (z p1 - z p2)^2
 
-part1 input =  final & map Set.size & sort & reverse & take 3
-    where combinations = [(p1, p2) | (p1:rest) <- tails input, p2 <-rest] & sort
+-- part1 :: [Point]
+part1 input = input
+    where combinations = 
+            [Set.fromList [p1, p2] | p1 <- input, p2 <- input, p1 /= p2]
+            & Set.fromList
+            & Set.map (\s -> Set.toList s)
+            & Set.toList
+            & sort
           distancesSquared = 
             map 
-                (\(p1@(x1, y1, z1), p2@(x2, y2, z2)) -> 
-                    ((x1-x2)^2 + (y1-y2)^2 + (z1-z2)^2, p1, p2))
+                (\[p1, p2] -> (distance p1 p2, p1, p2)) 
                 combinations
             & sort
             & take 10
-          final = foldl 
+          final = foldl
             (\memo (_, p1, p2)  -> mergePointsIntoSets memo p1 p2)
-            Set.empty 
+            Set.empty
             distancesSquared
-            
+
 main = do
     contents <- readFile "app/aoc/2025/8/input-mini.txt"
 
