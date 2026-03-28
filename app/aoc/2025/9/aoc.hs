22e36574bc4a1ae2eacb68078008adaef5274e6e
Canna Wen <cannawen@gmail.com>
2026-03-28 16:04:43 -0400

    Sets are kinda bad

--- app/aoc/2025/9/aoc.hs
+++ app/aoc/2025/9/aoc.hs
@@ -23,18 +23,24 @@ part1 input = [(p1, p2) | p1 <- input, p2 <- input, p1 < p2]
     & sort
     & last
 
-
 pointSet p1 p2 = Set.fromList [Point x y | x <- [(x p1) .. (x p2)], y <- [(min (y p1) (y p2)) .. (max (y p1) (y p2))]]
 
-part2 input = [(p1, p2) | p1 <- input, p2 <- input, p1 < p2]
-              & map (\(p1, p2) -> pointSet p1 p2)
-              & filter (\pointSet -> Set.disjoint pointSet (Set.fromList input))
-              & map Set.size
-              & sort
-              & last
-          
-          
+notBetween a b c = a < (min b c) || a > (max b c)
 
+createSet points p1 p2 = points
+    & filter (\p -> if x p == x p1 || x p == x p2
+                    then notBetween (y p) (y p1) (y p2)
+                    else if y p == y p1 || y p == y p2
+                    then notBetween (x p) (x p1) (x p2)
+                    else True)
+    & Set.fromList
+
+part2 input = [(p1, p2) | p1 <- input, p2 <- input, p1 < p2]
+              & filter (\(p1, p2) -> Set.disjoint (pointSet p1 p2) (createSet input p1 p2))
+              & map (uncurry pointSet)
+              & map Set.size
+              & maximum
+          
 main = do
     contents <- readFile "app/aoc/2025/9/input-mini.txt"
 
