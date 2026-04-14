e25d93f975c112a5113fc25e9f5029d5a0dbc3be
Canna Wen <cannawen@gmail.com>
2026-04-14 19:24:15 -0400

    This works as long as you give it the exact input it needs

--- todo.hs
+++ todo.hs
@@ -1,12 +1,18 @@
 import Control.Exception (evaluate)
 import Data.Function ((&))
 import System.IO
+import System.Environment
 
-main = deleteTodo
+main = do
+    (cmd:args) <- getArgs
+    dispatch cmd args
 
-deleteTodo = do
-    putStrLn "Which item would you like to delete?"
-    numberString <- getLine
+dispatch :: String -> [String] -> IO ()
+dispatch "add" = addTodo
+dispatch "delete" = deleteTodo
+dispatch _ = printTodo
+
+deleteTodo [numberString] = do
     contents <- readFile "todo.txt"
     
     _ <- evaluate (length contents)
@@ -16,7 +22,7 @@ deleteTodo = do
     
     writeFile "todo.txt" (unlines numberedItems)
 
-printTodo = do
+printTodo [] = do
     withFile "todo.txt" ReadMode (\handle -> do
         contents <- hGetContents handle
         let items = zip [0..] (lines contents)
@@ -24,8 +30,7 @@ printTodo = do
         mapM_ putStrLn numberedItems)
 
 
-addTodo = do 
-    todoItem <- getLine
+addTodo [todoItem] = do 
     withFile "todo.txt" AppendMode (\handle -> do
         hPutStrLn handle todoItem)
 
