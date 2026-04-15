52b1ded0a03298943ea71891c6eef115b308115b
Canna Wen <cannawen@gmail.com>
2026-04-15 13:26:58 -0400

    Cleanup

--- rpn.hs
+++ rpn.hs
@@ -2,16 +2,12 @@ import Data.Function ((&))
 import System.IO
 import System.Environment
 import Text.Read
-import Data.Maybe
 
 main = do
     args <- getArgs
     if null args
         then print (rpn "10 4 3 + 2 * -")
         else print (rpn (head args))
-
-isInt :: String -> Bool
-isInt s = isJust (readMaybe s :: Maybe Int)
 
 calculate ::[Int] -> String ->  [Int]
 calculate (a:b:ns) "+" = (b + a):ns
