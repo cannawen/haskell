4c348a80a3138c0c8dc38bd537167b0ea94b8f8c
Canna Wen <cannawen@gmail.com>
2026-04-15 15:17:52 -0400

    Cleanup

--- fastestPath.hs
+++ fastestPath.hs
@@ -1,20 +1,8 @@
-import Data.Function ((&))
-import System.IO
-import System.Environment
-import Text.Read
-import Text.Read.Lex (Number)
-
 data Road = A Int | B Int | C Int deriving (Show)
 
 roadNum (A n)  = n
 roadNum (B n)  = n
 roadNum (C n)  = n
-
-instance Eq Road where
-    (==) r1 r2 = roadNum r1 == roadNum r2
-
-instance Ord Road where
-    compare r1 r2 = compare (roadNum r1) (roadNum r2)
 
 type Segment = (Road, Road, Road)
 
@@ -25,11 +13,8 @@ a = map A [50, 5, 40, 10]
 b = map B [10, 90, 2, 8]
 c = map C [30, 20, 25, 0]
 
-segments :: [Road] -> [Road] -> [Road] -> [Segment]
-segments = zip3
-
 pathLength :: [Road] -> Int
-pathLength roads = foldl (\m r -> m + roadNum r) 0 roads
+pathLength = foldl (\m r -> m + roadNum r) 0
 
 fastestPathToA :: (ShortestPathToA, ShortestPathToB) -> Segment -> ShortestPathToA
 fastestPathToA path (a, b, c) = 
@@ -43,12 +28,14 @@ fastestPathToB path (a, b, c) =
         then b : snd path
         else c : a : fst path
 
---  B 10,  C 30,  A 5,  C 20,  B2,  B 8
+segmentTuples :: (ShortestPathToA, ShortestPathToB)
 segmentTuples = foldl 
     (\memo segment -> (fastestPathToA memo segment, fastestPathToB memo segment))
-    ([A 0], [B 0]) (segments a b c)
+    ([], []) (zip3 a b c)
 
+-- 75
+-- [B 10,C 30,A 5,C 20,B 2,B 8]
 main = do
-    print $ reverse (fst segmentTuples) 
+    print $ pathLength (fst segmentTuples) 
     print $ reverse (snd segmentTuples) 
 
