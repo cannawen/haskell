ab3b5d5d6bc4b7e11a0ff59f142d7b584d40b4b5
Canna Wen <cannawen@gmail.com>
2026-04-15 21:20:55 -0400

    Create some more lifted stuff

--- functor.hs
+++ functor.hs
@@ -2,16 +2,30 @@ import System.IO
 import Data.Char
 import Data.List
 
-main = upFunctor
+main = moreFunctor
 
 functor = do
     line <- fmap reverse getLine
     putStrLn $ "you said " ++ line ++ " backwards"
 
-upFunctor = do
+moreFunctor = do
     line <- fmap (intersperse '-' . map toUpper . reverse) getLine
     putStrLn $ "you said " ++ line ++ " backwards"
 
 noFunctor = do
     line <- getLine
     putStrLn $ "you said " ++ reverse line ++ " backwards"
+
+maybeReturner name = 
+    case name of 
+        Nothing -> Nothing 
+        Just n -> Just (length n)
+
+liftedMaybeReturner name = fmap length name
+
+data User = User { userId :: Int, userName :: String }
+
+getIds :: [User] -> [Int]
+getIds users = [userId u | u <- users]
+
+liftedGetIds users = fmap userId users
