58e7ff5a0a0bdedcbc9ab4383906f8792beb8f2d
Canna Wen <cannawen@gmail.com>
2026-04-20 16:10:07 -0400

    Using monad pattern

--- app/functor/monad.hs
+++ app/functor/monad.hs
@@ -2,8 +2,18 @@ import Data.Function
 type Bird = Int
 type Pole = (Int, Int)
 
-landLeft :: Bird -> Pole -> Pole
-landLeft n (l, r) = (n + l, r)
+landLeft :: Bird -> Pole -> Maybe Pole
+landLeft n (l, r) = createMaybePole (n + l, r)
 
-landRight :: Bird -> Pole -> Pole
-landRight n (l, r) = (l, n + r)
+landRight :: Bird -> Pole -> Maybe Pole
+landRight n (l, r) = createMaybePole (l, n + r)
+
+createMaybePole :: Pole -> Maybe Pole
+createMaybePole (l, r) = if abs (l - r) < 4 then Just (l, r) else Nothing
+
+
+main = do 
+    landLeft 1 (0,0)
+    >>= landRight 1
+    >>= landRight 5
+    & print
