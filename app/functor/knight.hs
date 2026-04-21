ddf140655871e6bbb19c625ce91a2ecade443bd6
Canna Wen <cannawen@gmail.com>
2026-04-21 14:25:44 -0400

    Fix how knight moves.

--- app/functor/knight.hs
+++ app/functor/knight.hs
@@ -7,17 +7,16 @@ type Direction = (Int, Int)
 boardSize = 8
 
 knightDirections :: [Direction]
-knightDirections = [ (x, y) | x <- posOrNeg <*> [2,5], y <- posOrNeg <*> [2,5], abs x /= abs y]
+knightDirections = [ (x, y) | x <- posOrNeg <*> [1,2], y <- posOrNeg <*> [1,2], abs x /= abs y]
     where posOrNeg = [id, negate]
 
-move :: Knight -> Direction -> Maybe Knight
-move (kx, ky) (dx, dy) =
-    if  kx + dx >= boardSize || kx + dx < 0 || ky + dy >= boardSize || ky + dy < 0 
-        then Nothing
-        else Just (kx + dx, ky + dy)
-
-moveKnight = map (\d -> move (6,1) d) knightDirections 
+move :: Knight -> [Knight]
+move (kx, ky) = do 
+    (dx, dy) <- knightDirections
+    guard (kx + dx < boardSize && kx + dx >= 0 && ky + dy < boardSize && ky + dy >= 0 )
+    return (kx + dx, ky + dy)
 
 main = do
     print knightDirections
-    print $ moveKnight
+    print $ move (5,1)
+    print $ move (7,0)
