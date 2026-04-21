f428170e310e45ab10a8770fb0ad9a7daa25a9b2
Canna Wen <cannawen@gmail.com>
2026-04-21 15:03:11 -0400

    Cleanup

--- app/functor/knight.hs
+++ app/functor/knight.hs
@@ -7,22 +7,34 @@ type Direction = (Int, Int)
 boardSize = 8
 
 knightDirections :: [Direction]
-knightDirections = [ (x, y) | x <- posOrNeg <*> [1,2], y <- posOrNeg <*> [1,2], abs x /= abs y]
-    where posOrNeg = [id, negate]
+knightDirections = [ (x, y) | 
+    x <- [id, negate] <*> [1,2], 
+    y <- [id, negate] <*> [1,2], 
+    abs x /= abs y ]
 
-move :: Knight -> [Knight]
-move (kx, ky) = do 
+moveA (kx, ky) = do 
     (dx, dy) <- knightDirections
     guard (kx + dx < boardSize && kx + dx >= 0 && ky + dy < boardSize && ky + dy >= 0 )
     return (kx + dx, ky + dy)
+moveB (kx, ky) = [ (kx + dx, ky + dy) | 
+    (dx, dy) <- knightDirections, 
+    kx + dx < boardSize, 
+    kx + dx >= 0, 
+    ky + dy < boardSize, 
+    ky + dy >= 0]
+
+move :: Knight -> [Knight]
+move = moveB
 
 inThreeMovesA start = do 
     oneMove <- move start
     twoMove <- move oneMove
     move twoMove
 inThreeMovesB start = return start >>= move >>= move >>= move
+inThreeMovesC start = move start >>= move >>= move
 
-inThreeMoves start = move start >>= move >>= move
+inThreeMoves :: Knight -> [Knight]
+inThreeMoves = inThreeMovesC
 
 canReachIn3 :: Knight -> Knight -> Bool
 canReachIn3 start end = end `elem` inThreeMoves start
