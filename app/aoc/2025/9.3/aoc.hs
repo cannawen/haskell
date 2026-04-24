ce7afc81b2299a61b4da9eee319c582289abad85
Canna Wen <cannawen@gmail.com>
2026-04-24 13:21:18 -0400

    Remove unused stuff

--- app/aoc/2025/9.3/aoc.hs
+++ app/aoc/2025/9.3/aoc.hs
@@ -9,7 +9,6 @@ import Control.Monad
 import qualified Data.Map as Map
 import qualified Control.Applicative as Map
 
--- type Point = (Int, Int, Int)
 data Point = Point
     { x :: Int
     , y :: Int
@@ -91,64 +90,54 @@ cornerPointsInRect s =  (
         Point (xMax s) (yMin s)
     ], s)
 
-parse :: String -> [Point]
-parse input =
-    map (((\arr -> Point (head arr) (last arr)) . (map read)) . splitOn ",") (lines input)
-
-shapeFromPoints :: [Point] -> [LineSegment]
-shapeFromPoints points = zip points (rotate points)
-    where rotate arr = tail arr ++ [head arr]
-
-rayCast:: LineSegment -> ShapePoints -> Int
-rayCast line shape =
-    foldl'
-    (\memo p -> if Set.member p shape then memo + 1 else memo)
-    0
-    (pointsFromSegment line)
-
-createRay :: Point -> Int -> LineSegment
-createRay p maxY = (p, Point (x p) maxY)
-
-isPointInShape :: Int -> ShapePoints -> ShapePoints -> Point -> Bool
-isPointInShape bound shapeH shapeV point =
-    Set.member point shapeH ||
-    Set.member point shapeV ||
-    odd (rayCast (createRay point bound) shapeV)
-
-arePointsInShape maxYBound shapePointsHorizontal shapePointsVertical = map (isPointInShape maxYBound shapePointsHorizontal shapePointsVertical)
-
 pointsFromSegment:: LineSegment -> [Point]
 pointsFromSegment (p1, p2) =
     if isHorizontal (p1, p2)
         then [Point (x p1) y | y <- [min (y p1) (y p2) .. max (y p1) (y p2)]]
         else [Point x (y p1) | x <- [min (x p1) (x p2) .. max (x p1) (x p2) & pred]]
 
+shapeFromPoints :: [Point] -> [LineSegment]
+shapeFromPoints points = zip points (rotate points)
+    where rotate arr = tail arr ++ [head arr]
 
-part2 input =  encode shapePointsVertical maxXBound-- insideRect
-    where
-        shape = shapeFromPoints input
-        shapePointsHorizontal = Set.unions (map (Set.fromList . pointsFromSegment) (filter isHorizontal shape))
-        shapePointsVertical = Set.unions (map (Set.fromList . pointsFromSegment) (filter (not . isHorizontal) shape))
+parse :: String -> [Point]
+parse input =
+    map (((\arr -> Point (head arr) (last arr)) . (map read)) . splitOn ",") (lines input)
 
-        -- insidePoints = Set.fromList [Point x y | x <- [0.. maxXBound], y <- [0..maxYBound], isPointInShape (Point x y) maxYBound shapePointsHorizontal shapePointsVertical]
+-- Part 1 ----------------------------------------------------------------------------------------------------------------
 
-        insideRect = [makeRect p1 p2 | p1 <- input, p2 <- input, p1 < p2]
-            & sortBy (comparing Down)
-            & map cornerPointsInRect
-            & filter (\(rectanglePoints, _) -> and (arePointsInShape maxYBound shapePointsHorizontal shapePointsVertical rectanglePoints))
-            & map (\(_, rect) -> borderPointsInRect rect)
-            & find (\(points, rect) -> and (arePointsInShape maxYBound shapePointsHorizontal shapePointsVertical points))
+part1 input =  
+    [(p1, p2) | p1 <- input, p2 <- input, p1 < p2]
+    & map (uncurry makeRect)
+    & sort
+    & reverse
 
-        maxXBound = map x input & maximum & succ
-        maxYBound = map y input & maximum & succ
+
+shapePointsHorizontal shape = Set.unions (map (Set.fromList . pointsFromSegment) (filter isHorizontal shape))
+shapePointsVertical shape = Set.unions (map (Set.fromList . pointsFromSegment) (filter (not . isHorizontal) shape))
+
+-- Part 2 ----------------------------------------------------------------------------------------------------------------
+
+
+pt2' shape input = pt2 shape (part1 input) (shapePointsHorizontal s) (shapePointsVertical s)
+    where s = shapeFromPoints input
+
+pt2 :: EncodedShape -> [Rect] -> ShapePoints -> ShapePoints -> Maybe Rect
+pt2 shape rect shapeH shapeV = 
+    find 
+    (\r -> 
+        True --TODO
+    ) rect
 
 main = do
     now <- getCurrentTime
     putStrLn (formatTime defaultTimeLocale "%A, %B %e, %Y - %H:%M:%S" now)
 
     contents <- readFile "app/aoc/2025/9/input.txt"
+    -- print $ part2 $ parse contents
 
-    writeFile "app/aoc/2025/9.3/output.txt" $ show $ part2 $ parse contents
+    savedShape <- readFile "app/aoc/2025/9.3/output.txt"
+    print $ pt2' (read savedShape :: EncodedShape) (parse contents)
 
     done <- getCurrentTime
     putStrLn (formatTime defaultTimeLocale "%A, %B %e, %Y - %H:%M:%S" done)
