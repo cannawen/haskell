8bc7cac01797b17396a66895e58e86a2e360defc
Canna Wen <cannawen@gmail.com>
2026-04-27 15:12:19 -0400

    Alternate possibility

--- app/functor/writer.hs
+++ app/functor/writer.hs
@@ -42,6 +42,7 @@ gcd' a b
     | b == 0 = do
         tell ["Finished with  " ++ show a]
         return a
+    -- | b == 0 = writer (a, ["Finished with  " ++ show a])
     | otherwise = do
         tell [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]
         gcd' b (a `mod` b)
