1c6396960fd1fbcb4b6c6014be25ce5ad242f043
Canna Wen <cannawen@gmail.com>
2026-04-28 12:00:13 -0400

    Solving using monads.... hmmm.

--- app/functor/rpn.hs
+++ app/functor/rpn.hs
@@ -1,10 +1,26 @@
 import Data.List
+import Control.Monad
 
-solveRPN :: String -> Double
-solveRPN = head . foldl foldingFn [] . words
+solveRPN' :: String -> Double
+solveRPN' = head . foldl foldingFn' [] . words
 
-foldingFn :: [Double] -> String -> [Double]
-foldingFn (x:y:ys) "*" = (y * x):ys
-foldingFn (x:y:ys) "+" = (y * x):ys
-foldingFn (x:y:ys) "-" = (y * x):ys
-foldingFn xs numString = read numString:xs
+foldingFn' :: [Double] -> String -> [Double]
+foldingFn' (x:y:ys) "*" = (y * x):ys
+foldingFn' (x:y:ys) "+" = (y * x):ys
+foldingFn' (x:y:ys) "-" = (y * x):ys
+foldingFn' xs numString = read numString:xs
+
+solveRPN :: String -> Maybe Double
+solveRPN st = do
+    [result] <- foldM foldingFn [] $ words st
+    return result
+
+foldingFn :: [Double] -> String -> Maybe [Double]
+foldingFn (x:y:ys) "*" = Just $ (y * x):ys
+foldingFn (x:y:ys) "+" = Just $ (y * x):ys
+foldingFn (x:y:ys) "-" = Just $ (y * x):ys
+foldingFn xs numString = liftM (:xs) (readMaybe numString)
+
+readMaybe :: (Read a) => String -> Maybe a
+readMaybe st = case reads st of [(x, "")] -> Just x
+                                _ -> Nothing
